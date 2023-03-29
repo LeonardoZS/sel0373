1a3e70bdb6503491bac1fbbc9425788a66921291
Bruno <brunosant2001@gmail.com>
2023-03-29 17:55:20 -0300

    Adicionado o ler_cadastro

--- Cadastro/cadastro.py
+++ Cadastro/cadastro.py
@@ -4,10 +4,27 @@ from firebase_admin import credentials, initialize_app, storage, firestore
 import os 
 
 # Init firebase with your credentials
-cred = credentials.Certificate("key.json") #tem que baixar a chave e deixar na mesma pasta do arquivo ou então mudar o caminho
+cred = credentials.Certificate("Cadastro/key.json") #tem que baixar a chave e deixar na mesma pasta do arquivo ou então mudar o caminho
 initialize_app(cred, {'storageBucket': 'porteiroeletronico-sel0373.appspot.com'}) #inicializa no nosso banco de dados
 db = firestore.client()
 #collection = db.collection('places') 
+
+def ler_token():
+
+    comando_ref = db.collection(u'Token').document(u'token')
+    comando_dict = comando_ref.get().to_dict()
+    token = comando_dict['token']
+    nome = comando_dict['nome']
+
+    if token == 1:
+        cadastro(nome)
+    
+    if token == 2:
+        remover(nome)
+
+    db.collection(u'Token').document('token').set({u'name': "" , u'token': 0})
+
+    return
 
 def captura_imagem(token, name): #capturar a imagem
     vid = cv2.VideoCapture(0)     
@@ -30,8 +47,7 @@ def upload_and_get_url(fileName): #passa um arquivo e faz upload
     blob.make_public() #torna a imagem publica
     return blob.public_url #retorna a url
 
-def cadastro(): #função de cadastro
-    name = input("Digite o nome de cadastro:") #recebe o nome do cadastro
+def cadastro(name): #função de cadastro
     image_name = f"{name}.jpg" #formata o nome da imagem que vai ser enviada pro storage
     captura_imagem(1, name) #tira a foto da imagem, com token = 1 e com o nome passado
     url = upload_and_get_url(image_name) #pega a url a partir da função feita pra isso
@@ -53,6 +69,8 @@ def remover(name): #remover do storage e do firestore, o nome da pessoa é passa
     blob = bucket.blob(name + ".jpg") #passa o nome da imagem pro bucket
     blob.delete() #remove o blob com o nome da imagem
     return print("cadastro e imagem removidas com sucesso")
+
+ler_token()
 
 ##########LINKS UTEIS##############
 #https://firebase.google.com/s/results/?q=db%20collection
