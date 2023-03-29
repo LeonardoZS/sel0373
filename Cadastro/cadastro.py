0220ee54072670c85b25e5525bf4fdd8afdc94eb
Gabriel Domingues <62193410+domingues100@users.noreply.github.com>
2023-03-29 16:12:19 -0300

    Update cadastro.py

--- Cadastro/cadastro.py
+++ Cadastro/cadastro.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 from firebase_admin import credentials, initialize_app, storage, firestore
+import os
 
 # Init firebase with your credentials
 cred = credentials.Certificate("key.json")
@@ -8,12 +9,18 @@ initialize_app(cred, {'storageBucket': 'porteiroeletronico-sel0373.appspot.com'}
 db = firestore.client()
 collection = db.collection('places')
 
-def captura_imagem(): #capturar a imagem
+def captura_imagem(token): #capturar a imagem
     vid = cv2.VideoCapture(0)     
     time.sleep(1)
     _, imagem = vid.read()
     vid.release()
-    cv2.imwrite("teste.jpg", imagem)
+
+    if token == 1:
+        file_name = "teste.jpg"
+    else:
+        file_name = "video.jpg"
+
+    cv2.imwrite(file_name, imagem)
     return 
 
 def upload_and_get_url(fileName): #passa um arquivo e faz upload
@@ -24,11 +31,25 @@ def upload_and_get_url(fileName): #passa um arquivo e faz upload
     return blob.public_url
 
 def cadastro(): #função de cadastro
-    captura_imagem()
+    image_name = "teste.jpg"
+    captura_imagem(1)
     name = input("Digite o nome de cadastro:")
-    image = "teste.jpg"
-    url = upload_and_get_url(image)
-    update_time, city_ref = db.collection(u'cadastros').add({u'name': name , u'foto': url}) #.add() é sem id
+    url = upload_and_get_url(image_name)
+    os.remove(image_name)
+    db.collection(u'cadastros').add({u'name': name , u'foto': url}) #.add() é sem id
     return print("cadastro concluido com sucesso")
 
+def real_time_image(): #função para ficar capturando a imagem e ficar enviando para a coleção "video" que será mostrada no site
+    image_name = "video.jpg"
+    imagem = captura_imagem(2) #usa a função captura a imagem
+    url = upload_and_get_url(image_name) #usa a função de guardar a imagem e obter a url
+    os.remove(image_name)
+    db.collection(u'video').document(u'imagem').set({u'name': "video" , u'foto': url}) #joga a imagem na coleação
+    return
+
 cadastro()
+
+##########LINKS UTEIS##############
+#https://firebase.google.com/s/results/?q=db%20collection
+#https://firebase.google.com/docs/firestore/query-data/listen?hl=pt-br
+#https://firebase.google.com/docs/firestore/manage-data/add-data?hl=pt-br
