29ac0e58db997c1feac392e85ac64b4c7a59200d
Gabriel Domingues <62193410+domingues100@users.noreply.github.com>
2023-05-03 17:33:11 -0300

    Update cadastro.py

--- Cadastro/cadastro.py
+++ Cadastro/cadastro.py
@@ -105,7 +105,7 @@ def known_face_encodings(directory):
     return encodings
 
 def reconhecimento():
-    image = face_recognition.load_image_file("leo.jpg")
+    image = face_recognition.load_image_file("cadastro/leo.jpg")
     face_locations = face_recognition.face_locations(image) 
     face_encodings = face_recognition.face_encodings(image, face_locations)
 
